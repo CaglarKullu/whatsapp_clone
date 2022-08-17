9e0515ceea2c3e30e78471acd9bbabc8bd37f14c
CaglarKullu <caglarkullu@gmail.com>
2022-08-17 18:58:13 +0100

    first commit

--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -1,12 +1,62 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:whatsapp_clone/consts/color.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text("Mobile");
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: appBarColor,
+            elevation: 0,
+            centerTitle: false,
+            title: const Text(
+              'Whatsapp',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                color: greyColor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.search,
+                    color: greyColor,
+                  )),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.more_vert,
+                    color: greyColor,
+                  )),
+            ],
+            bottom: const TabBar(
+                indicatorColor: tabColor,
+                indicatorWeight: 4,
+                labelColor: tabColor,
+                unselectedLabelColor: greyColor,
+                labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                tabs: [
+                  Tab(
+                    text: "CHATS",
+                  ),
+                  Tab(
+                    text: "STATUS",
+                  ),
+                  Tab(
+                    text: "CALLS",
+                  ),
+                ]),
+          ),
+        ));
   }
 }
